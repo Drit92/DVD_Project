29bc43aa67e3a7388a8d3123ec08ce8b16ce522b
Drit92 <146371382+Drit92@users.noreply.github.com>
2025-12-13 17:25:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -425,7 +425,7 @@ if len(radar_cols) >= 3:
     angles = np.linspace(0, 2 * np.pi, len(labels), endpoint=False).tolist()
     angles += angles[:1]  # close loop
 
-    fig_radar, ax_radar = plt.subplots(figsize=(7, 7), subplot_kw=dict(polar=True))
+    fig_radar, ax_radar = plt.subplots(figsize=(4, 4), subplot_kw=dict(polar=True))
 
     # Non-defaulters (TARGET=0)
     if 0 in radar_norm.columns:
@@ -450,7 +450,7 @@ if len(radar_cols) >= 3:
     ax_radar.legend(bbox_to_anchor=(1.25, 1.1))
     plt.tight_layout()
 
-    st.pyplot(fig_radar, width="stretch")
+    st.pyplot(fig_radar, width="content")
 
     st.markdown("""
     **Story:**
